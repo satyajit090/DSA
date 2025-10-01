73bd3347de698ba957c6945b2eb49d0ae114972c
satyajit090 <beherasatyajit090@gmail.com>
2025-10-01 23:38:52 +0530

    linkedlist Cycle solution

---
+++ InterviewPractice/LinkedListCycle.java
@@ -0,0 +1,46 @@
+package InterviewPractice;
+
+class  ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val=x;
+        next=null;
+    }
+}
+
+public class LinkedListCycle {
+
+    public static void main(String[] args) {
+//        System.out.println("Satyajit");
+//        ListNode head=new ListNode(0);
+//        boolean m=hasCycle(head);
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+
+        System.out.println("Has Cycle? " + hasCycle(head1)); // false
+
+        // Example 2: Linked List with cycle
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(2);
+        head2.next.next = new ListNode(3);
+        head2.next.next.next = head2; // creates cycle
+
+        System.out.println("Has Cycle? " + hasCycle(head2)); // true
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+
+        while(fast !=null && fast.next != null){
+                fast=fast.next.next;
+                slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
